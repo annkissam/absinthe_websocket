54652aff269693186249b89413eb46d8c8ca49bd
Eric Sullivan <eric.sullivan@annkissam.com>
2018-04-24 11:32:28 -0400

    fix poison dependency

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AbsintheWebsocket.Mixfile do
   def project do
     [
       app: :absinthe_websocket,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       deps: deps(),
