b10b06aea80da1252a7445f9a09700533a6b7fe1
Brett Lyman <github@brettlyman.com>
2019-03-21 11:44:21 -0600

    Queue subscribe/unsubscribe casted messages while disconnected, re-cast them after joined again

--- lib/absinthe_websocket/subscription_server.ex
+++ lib/absinthe_websocket/subscription_server.ex
@@ -6,6 +6,8 @@ defmodule AbsintheWebSocket.SubscriptionServer do
     socket = Keyword.get(args, :socket)
     subscriber = Keyword.get(args, :subscriber)
     state = %{
+      connected?: false,
+      disconnected_casts: [],
       socket: socket,
       subscriber: subscriber,
       subscriptions: %{},
@@ -25,12 +27,24 @@ defmodule AbsintheWebSocket.SubscriptionServer do
     GenServer.cast(mod, {:unsubscribe, subscription_name})
   end
 
+  def handle_cast({:subscribe, _, _, _, _} = message, %{connected?: false} = state) do
+    state = Map.put(state, :disconnected_casts, state.disconnected_casts ++ [message])
+
+    {:noreply, state}
+  end
+
   def handle_cast({:subscribe, subscription_name, callback, query, variables}, %{socket: socket, subscriptions: subscriptions} = state) do
     AbsintheWebSocket.WebSocket.subscribe(socket, self(), subscription_name, query, variables)
 
     callbacks = Map.get(subscriptions, subscription_name, [])
     subscriptions = Map.put(subscriptions, subscription_name, [callback | callbacks])
     state = Map.put(state, :subscriptions, subscriptions)
+
+    {:noreply, state}
+  end
+
+  def handle_cast({:unsubscribe, _} = message, %{connected?: false} = state) do
+    state = Map.put(state, :disconnected_casts, state.disconnected_casts ++ [message])
 
     {:noreply, state}
   end
@@ -57,6 +71,13 @@ defmodule AbsintheWebSocket.SubscriptionServer do
   def handle_cast({:joined}, %{subscriber: subscriber} = state) do
     apply(subscriber, :subscribe, [])
 
+    Enum.each(state.disconnected_casts, &GenServer.cast(self(), &1))
+    state = Map.merge(state, %{connected?: true, disconnected_casts: []})
+
     {:noreply, state}
+  end
+
+  def handle_cast({:disconnected}, state) do
+    {:noreply, Map.put(state, :connected?, false)}
   end
 end
